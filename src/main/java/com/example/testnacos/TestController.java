f53d8a50e6e99b06a9a78a96e9df4d2905399da2
jin.wang <wangjin-252@hotmail.com>
2019-07-22 18:00:19 +0800

    init commit

---
+++ src/main/java/com/example/testnacos/TestController.java
@@ -0,0 +1,25 @@
+package com.example.testnacos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jin.Wang
+ * @version 1.0
+ * @date 2019-07-21 11:07
+ */
+@RestController
+@RefreshScope
+public class TestController {
+
+
+    @Value(value = "${teststr:defalut}")
+    private String testStr;
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return testStr;
+    }
+}
---
+++ src/main/java/com/example/testnacos/TestNacosApplication.java
@@ -0,0 +1,13 @@
+package com.example.testnacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestNacosApplication.class, args);
+    }
+
+}
